8cd0bd4bf9f536d23e60af7bed3d8ae818074289
Kathy Bauer <kbauer17@my.wctc.edu>
2023-08-29 08:31:54 -0500

    [2.08] Play sound effect on toast

--- client.js
+++ client.js
@@ -1,6 +1,10 @@
 $(function(){
+        // preload audio
+        var toast = new Audio('toast.wav');
     $('.code').on('click', function(e) {
         e.preventDefault();
+            // play audio
+            toast.play();
         $('#liveToast').toast({ autohide: false }).toast('show');
     });
 });
