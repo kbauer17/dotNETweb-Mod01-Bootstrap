3d60ae0ab632af776c7df18767696def678a4db5
Kathy Bauer <kbauer17@my.wctc.edu>
2023-08-29 08:33:19 -0500

    [2.09] Pause and reset audio before playing

--- client.js
+++ client.js
@@ -3,6 +3,10 @@ $(function(){
         var toast = new Audio('toast.wav');
     $('.code').on('click', function(e) {
         e.preventDefault();
+                // first pause the audio (in case it is still playing)
+                toast.pause();
+                // reset the audio
+                toast.currentTime = 0;
             // play audio
             toast.play();
         $('#liveToast').toast({ autohide: false }).toast('show');
