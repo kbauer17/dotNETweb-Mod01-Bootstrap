5a5fc9e0da19f41437ea7fc23a987edd41f2cb89
Kathy Bauer <kbauer17@my.wctc.edu>
2023-08-29 10:58:07 -0500

    [3.03] Display product name, code in toast -use data- attributes

--- client.js
+++ client.js
@@ -3,21 +3,21 @@ $(function(){
         var toast = new Audio('toast.wav');
     $('.code').on('click', function(e) {
         e.preventDefault();
-                // first pause the audio (in case it is still playing)
-                toast.pause();
-                // reset the audio
-                toast.currentTime = 0;
+          // first pause the audio (in case it is still playing)
+          toast.pause();
+          // reset the audio
+          toast.currentTime = 0;
             // play audio
             //toast.play();
         $('#liveToast').toast({ autohide: false }).toast('show');
+        $('#header').html($(this).data('header'));
+        $('#code').html($(this).data('code'));
     });
 });
 
-var toastEscape = document.querySelector('#liveToast')
-
 window.addEventListener('keydown', function (event) {
   if (event.key === 'Escape') {
-    toastEscape.style.display = 'none'
+    $('#liveToast').toast('hide')
   }
 })
 
