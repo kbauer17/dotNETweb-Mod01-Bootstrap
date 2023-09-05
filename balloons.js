e10c0cb565120f1cbb307eb3b50df9b33d2162b3
Kathy Bauer <kbauer17@my.wctc.edu>
2023-09-05 08:27:28 -0500

    [4.09] Hide/show balloons when checkboxes toggled

--- balloons.js
+++ balloons.js
@@ -4,4 +4,13 @@ $(function(){
     $('.form-check-input').each(function () {
         $(this).prop('checked', false);
     });
+    // event listener for check/uncheck
+    $('.form-check-input').on('change', function () {
+        // make the image visible
+        $('#' + this.id + 'Img').css('visibility', 'visible')
+        // animate balloon in/out based on checkbox
+        $(this).is(':checked') ?
+         $('#' + this.id + 'Img').removeClass().addClass('animate__animated animate__bounceInDown') :
+         $('#' + this.id + 'Img').addClass('animate__animated animate__bounceOutUp');
+    });
 });
