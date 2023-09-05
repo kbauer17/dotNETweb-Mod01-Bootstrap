f0575f60724a36735814e941cacf6d8a3c6a7b34
Kathy Bauer <kbauer17@my.wctc.edu>
2023-09-05 11:22:33 -0500

    [4.12] Add btns to select all/no balloons

--- balloons.js
+++ balloons.js
@@ -15,6 +15,7 @@ $(function(){
     });
 });
 
+// Function to display toast
 $(document).ready(function(){
     $(".show-toast").click(function(){
         $("#myToast").toast({ autohide: false });
@@ -26,3 +27,31 @@ $(document).ready(function(){
 function changeHeadingColor(color){
     document.getElementById("heading").style.color = color;
 }
+
+// function to select all balloons
+$(document).ready(function(){
+    $.fn.selectAllBalloons = function(){ 
+        $('.form-check-input').each(function () {
+            $(this).prop('checked', true);
+            $('#' + this.id + 'Img').removeClass().addClass('animate__animated animate__bounceInDown');
+        });
+         
+    }
+    $(".btn-selectAll").click(function(){
+        $.fn.selectAllBalloons();
+    });
+});
+
+// function to de-select all balloons
+$(document).ready(function(){
+    $.fn.selectNoBalloons = function(){ 
+        $('.form-check-input').each(function () {
+            $(this).prop('checked', false);
+            $('#' + this.id + 'Img').addClass('animate__animated animate__bounceOutUp');
+        });
+         
+    }
+    $(".btn-selectNone").click(function(){
+        $.fn.selectNoBalloons();
+    });
+});
